bb6a329e617514fdd8753e9ee6028a2fb0749c07
Ania <muniolka@vp.pl>
2015-05-13 02:59:47 +0200

    Finisz

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
+	has_many :comments, dependent: :destroy #jak usuniesz posta to z komentarzami
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
 end
